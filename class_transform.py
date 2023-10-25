4d8f5e2a0e4e398883f669e2810549bf026f9d85
Bach Antoine <118629459+AntoineBch29@users.noreply.github.com>
2023-10-25 11:13:32 +0200

    update transform

--- class_transform.py
+++ class_transform.py
@@ -1,69 +1,89 @@
+from typing import Any
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 class stft(object):
-    def __init__(self, y, n_y, n_fft, hop_length):
-        self.y = y
-        self.n_y = n_y
+    def __init__(self, n_fft, hop_length, win_length):
         self.n_fft = n_fft
         self.hop_length = hop_length
-    def __call__(self):
-        y_pad = librosa.util.fix_length(
-            self.y, size=self.n_y + self.n_fft // 2)
-        return librosa.stft(y_pad, n_fft=self.n_fft, hop_length=self.hop_length)
-
-class power(object):
-    def __init__(self, y):
-        self.y = y
-    def __call__(self):
-        return np.abs(self.y)**2
-
-class mask(object):
-    def __init__(self, u, s):
-        self.u = u
-        self.s = s
-    def __call__(self):
-        return self.s > self.u
-
-class log_amplitude(object):
-    def __init__(self, P):
-        self.P = P
-    def __call__(self):
-        return np.log(self.P)
-
+        self.win_length = win_length
+    def __call__(self,sample):
+        y_pad= librosa.util.fix_length(
+        sample["Waveform"], size=len(sample["Waveform"]) + self.n_fft // 2)
+        return librosa.stft(y_pad, n_fft=self.n_fft, hop_length=self.hop_length, win_length=self.win_length, sr=sample["Sample_rate"])
+class fourier_signal_and_mask_power(object):
+    def __init__(self,n_fft, hop_length,win_length):
+        self.stft_object=stft(n_fft, hop_length,win_length)
+    def __call__(self, sample):
+        sample["X"]=np.log10(np.abs(self.stft_object(sample["Noised_Waveform"]))**2)
+        sample["Y"]=np.abs(self.stft_object(sample["Waveform"]))**2>np.abs(self.stft_object(sample["Noise"]))**2
+        return sample
 class istft(object):
-    def __init__(self, y, n_y, n_fft, hop_length):
-        self.y = y
-        self.n_y = n_y
+    def __init__(self, n_fft, hop_length, win_length):
         self.n_fft = n_fft
         self.hop_length = hop_length
+        self.win_length = win_length
     def __call__(self):
-        return librosa.istft(self.y, hop_length=self.hop_length, n_fft=self.n_fft, length=self.n_y)
-
-
-y, sr = librosa.load(librosa.ex("C:/Users/pemba/Documents/GitHub/Data/LibriSpeech/train-clean-100/125/121342/125-121342-0000.flac"))
-p = stft(y, len(y), 2048, 1024)
-k=p()
-power_amp=power(k)
-S = power_amp()
-u, sr2 = librosa.load("C:/Users/pemba/Downloads/babble_16k.wav")
-p2 = stft(u, len(u), 2048, 1024)
-k2=p2()
-power_amp2=power(k2)
-S2 = power_amp2()
-m=mask(S2[:,0:116],S)
-fig, ax = plt.subplots()
-img = librosa.display.specshow(librosa.power_to_db(m(), ref=np.max), y_axis='log', x_axis='time', ax=ax)
-ax.set_title('Power spectrogram')
-fig.colorbar(img, ax=ax, format="%+2.0f dB")
-plt.show()
-fig, ax = plt.subplots()
-img = librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax)
-ax.set_title('Power spectrogram')
-fig.colorbar(img, ax=ax, format="%+2.0f dB")
-plt.show()
-j = istft(k, len(y), 2048, 1024)
-y_out = j()
-
+        return librosa.istft(sample["Waveform"], hop_length=self.hop_length, n_fft=self.n_fft, win_length=self.win_length, length=len(sample["Waveform"]),sr=sample["Sample_rate"])
+class normalize(object):
+    def __call__(self,y):
+        return (y-np.mean(y))/np.max(np.abs(np.min(y)),np.abs(np.max(y)))  
+class noised_waveform(object):
+    def __init__(self, range):
+        self.range = range
+    def __call__(self,sample):
+        randomNums = np.random.poisson(1.5, 1)
+        randomInts = np.round(randomNums)
+        if randomInts<1:
+            randomInts=1
+        sum_alpha=0
+        for i in range(randomInts):
+            alpha=np.random()
+            sum_alpha+=alpha
+            crop_noise=np.random.randint
+            sample["Noise"]+=
+        sample["Noise"]=    
+        return sample
+    
+    
+    
+    
+if __name__ == "__main__" :
+    from Data.Datamodule.Dataset import DatasetLibrispeech
+    dataset=DatasetLibrispeech()
+    sample=dataset[0]
+    y=sample["Waveform"].numpy().T
+    sr=sample["Sample_rate"]
+    y=y[:,0]
+    p = stft(y, len(y), 2048, 1024)
+    k=p()
+    power_amp=power(k)
+    S = power_amp()
+    u, sr2 = librosa.load("C:/Users/pemba/Downloads/babble_16k.wav",sr=16000)
+    print(len(u),sr)
+    p2 = stft(u, len(u), 2048, 1024)
+    k2=p2()
+    power_amp2=power(k2)
+    S2 = power_amp2()
+    m=mask(S2[:,1:94],S)
+    fig, ax = plt.subplots(ncols=3)
+    print("coucou")
+    img = librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax[0])
+    ax[0].set_title('signal ')
+    fig.colorbar(img, ax=ax[0], format="%+2.0f dB")
+    img = librosa.display.specshow(librosa.power_to_db(S2, ref=np.max), y_axis='log', x_axis='time', ax=ax[1])
+    ax[1].set_title('noise')
+    fig.colorbar(img, ax=ax[1], format="%+2.0f dB")
+    img = librosa.display.specshow(librosa.power_to_db(m(), ref=np.max), y_axis='log', x_axis='time', ax=ax[2])
+    ax[2].set_title('mask')
+    fig.colorbar(img, ax=ax[2], format="%+2.0f dB")
+    plt.show()
+    j = istft(k, len(y), 2048, 1024)
+    y_out = j()
+    # fig=plt.figure(2)
+    # randomNums = np.random.poisson(1.5, 10000)
+    # randomInts = np.round(randomNums)
+    # randomInts = np.clip(randomInts, 1, 10)
+    # count, bins, ignored = plt.hist(randomInts, 14, density=True)
+    # plt.show()
